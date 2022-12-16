113a141189de8026606cf39dd69380231b2bd8cd
Daniel Bedoya Alzate <daniel.alzate@ivoryit.com.br>
2022-12-15 21:16:18 -0300

    End of section 8

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-home',
@@ -10,22 +11,13 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private membersService: MembersService) { }
 
-  ngOnInit(): void {
-    this.getUsers();
+  ngOnInit(): void { 
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    });
   }
 
   cancelRegisterMode(event: boolean) {
